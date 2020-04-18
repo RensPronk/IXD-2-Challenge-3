adaa12cb1a762008882791b5341a541589001ae2
RensPronk <37616951+RensPronk@users.noreply.github.com>
2020-04-18 19:58:49 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,66 @@
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmVuc3Byb25rIiwiYSI6ImNrOHp0YWx2dzFrc2IzaHM3aHB2dmNwcHoifQ._6TFivpXT2bIr3a3-pOUsQ'; //Token to use the mapbox api
+
+		const ISSurl = 'https://api.wheretheiss.at/v1/satellites/25544'; //create a const from the api URL
+		async function ISSlocation() { //make the function to fetch the data from the api URL
+			const response = await fetch(ISSurl);
+			const data = await response.json();
+			const { latitude, longitude } = data; 
+
+	var map = new mapboxgl.Map({ //create the mapbox map
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [latitude, 0], // startin position off the map. Due to the API being unstable it won't always let you see the Lat and Long so choose one of them and give the other one a value.
+		zoom: 2 //zoomlevel off the map.
+	});
+ 
+	map.on('load', function() { //make an onload function where everytime it loads it will generate an image as an icon on the map.
+		map.loadImage(
+			'https://i0.wp.com/freepngimages.com/wp-content/uploads/2015/12/international-space-station-transparent-background.png?fit=817%2C325',
+
+			function(error, image) {
+				if (error) throw error;
+				map.addImage('ISS', image);
+				map.addSource('point', {
+					'type': 'geojson',
+					'data': {
+						'type': 'FeatureCollection',
+						'features': [
+							{
+								'type': 'Feature',
+								'geometry': {
+									'type': 'Point',
+									'coordinates': [latitude, longitude] // the position of the image will be the exact position off the ISS. It will fetch the Lat and Long everytime the page loads.
+								}
+							}
+						]
+					}
+				});
+
+
+				map.addLayer({
+				'id': 'points',
+				'type': 'symbol',
+				'source': 'point',
+				'layout': {
+					'icon-image': 'ISS',
+					'icon-size': 0.20
+				}
+			});
+		}
+	);
+});
+
+		// map.setView([latitude,longitude], 1);
+		document.getElementById('lat').textContent = latitude.toFixed(2); //on the bottom it will display the Latitude rounded up to 2 decimals.
+		document.getElementById('lon').textContent = longitude.toFixed(2); //on the bottom it will display the Longitude rounded up to 2 decimals.
+		console.log(data.latitude); //it will write the lat and log to the console for troubleshooting.
+		console.log(data.longitude);
+	}
+
+
+
+
+	ISSlocation(); //Function that will fetch the Location off the ISS from the API
+	setInterval(ISSlocation, 10000); //The page will update every 10 seconds.
